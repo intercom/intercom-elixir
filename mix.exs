3ad8555dae7e54460374657c69d56f0c89eb946d
Almir Sarajčić <almir.sarajcic@icloud.com>
2021-05-14 19:11:55 +0200

    Fix missing module error in Elixir release

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Intercom.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:estree, :logger]]
   end
 
   # Dependencies can be Hex packages:
