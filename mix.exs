8b4a5fa284c77eed093bace0f9b294d2ff3c4ed3
Almir Sarajčić <almir.sarajcic@icloud.com>
2021-05-21 08:05:38 +0200

    Fix warning

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Intercom.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      package: package(),
-     deps: deps]
+     deps: deps()]
   end
 
   # Configuration for the OTP application
